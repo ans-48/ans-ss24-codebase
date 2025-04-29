cb22467f902ab3ed7a044896f5b6de52ec8444f8
Selorm Lotsu <selorml@mail.uni-paderborn.de>
2025-04-29 15:15:13 +0200

    Implement simple network

--- lab0/network_topo.py
+++ lab0/network_topo.py
@@ -1,22 +1,28 @@
 """
- Copyright 2024 Computer Networks Group @ UPB
+ Copyright (c) 2025 Computer Networks Group @ UPB
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
 
-      https://www.apache.org/licenses/LICENSE-2.0
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  """
 
 #!/usr/bin/python
 
 from mininet.topo import Topo
+from mininet.link import TCLink
 
 class BridgeTopo(Topo):
     "Creat a bridge-like customized network topology according to Figure 1 in the lab0 description."
@@ -26,5 +32,14 @@ class BridgeTopo(Topo):
         Topo.__init__(self)
 
         # TODO: add nodes and links to construct the topology; remember to specify the link properties
+        switches = [ self.addSwitch('s%s' % (s+1)) for s in range(2) ]
+        hosts = [ self.addHost('h%s' % (h+1)) for h in range(4) ]
+
+        for n in range(2):
+            self.addLink(switches[0], hosts[n], cls=TCLink, bw=15, delay=10)
+        for n in range(2, 4):
+            self.addLink(switches[1], hosts[n], cls=TCLink, bw=15, delay=10)
+
+        self.addLink(switches[0], switches[1], cls=TCLink, bw=20, delay=45)
 
 topos = {'bridge': (lambda: BridgeTopo())}
