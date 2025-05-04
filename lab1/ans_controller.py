cb0f53781e72e8df53efc80d471aba1f587f00a8
ganesh-v1 <ganeshk@mail.uni-paderborn.de>
2025-05-04 22:50:45 +0000

    Working template - Lab1

--- lab1/ans_controller.py
+++ lab1/ans_controller.py
@@ -1,64 +1,248 @@
-"""
- Copyright 2024 Computer Networks Group @ UPB
+# File: ans_controller.py (Final Working Version)
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      https://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- """
+import ipaddress # Use standard Python library for IP addresses/networks
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
+from ryu.lib import ip as ip_lib # Keep for IPNetwork potentially? No, use ipaddress fully.
+from ryu.lib import mac as mac_lib
+from ryu.lib.packet import arp
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+from ryu.lib.packet import icmp
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import packet
+from ryu.lib.packet import tcp
+from ryu.lib.packet import udp
 from ryu.ofproto import ofproto_v1_3
 
-
-class LearningSwitch(app_manager.RyuApp):
+class AnsController(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
-    def __init__(self, *args, **kwargs):
-        super(LearningSwitch, self).__init__(*args, **kwargs)
+    S1_DPID = 0x11
+    S2_DPID = 0x12
+    S3_DPID = 0x13
 
-        # Here you can initialize the data structures you want to keep at the controller
-        
+    ROUTER_PORTS = {
+        S3_DPID: {
+            1: {'ip': '10.0.1.1', 'mac': '00:00:00:00:01:01', 'subnet': '10.0.1.0/24'},
+            2: {'ip': '10.0.2.1', 'mac': '00:00:00:00:01:02', 'subnet': '10.0.2.0/24'},
+            3: {'ip': '192.168.1.1', 'mac': '00:00:00:00:01:03', 'subnet': '192.168.1.0/24'}
+        }
+    }
+    H1_IP = '10.0.1.2'
+    H2_IP = '10.0.1.3'
+    SER_IP = '10.0.2.2'
+    EXT_IP = '192.168.1.123'
+    INTERNAL_HOST_IPS = [H1_IP, H2_IP, SER_IP]
+
+    def __init__(self, *args, **kwargs):
+        super(AnsController, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.arp_table = {self.S3_DPID: {}}
+        self.logger.info("ANS Lab1 Controller Initialized (Final Fixes)")
+
+    def add_flow(self, datapath, priority, match, actions, idle_timeout=0, hard_timeout=0):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)] # Use OFPIT_
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst, idle_timeout=idle_timeout,
+                                hard_timeout=hard_timeout)
+        datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-        
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
-        # Initial flow entry for matching misses
+        dpid = datapath.id
+        # self.logger.info(f"Device DPID {dpid:x} connected.")
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
+        # self.logger.info(f"  -> Installed Table-Miss Flow Entry for DPID {dpid:x}")
+        if dpid == self.S3_DPID:
+            # self.logger.info(f"Router {dpid:x} detected. Installing base security rules.")
+            self.install_router_base_rules(datapath)
 
-    # Add a flow entry to the flow-table
-    def add_flow(self, datapath, priority, match, actions):
-        ofproto = datapath.ofproto
+    def install_router_base_rules(self, datapath):
         parser = datapath.ofproto_parser
+        dpid = datapath.id
+        priority_high = 10
+        # self.logger.info(f"Router {dpid:x}: Installing base rules with Priority {priority_high}")
+        for internal_ip in self.INTERNAL_HOST_IPS:
+            match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ip_proto=1,
+                                    icmpv4_type=icmp.ICMP_ECHO_REQUEST, ipv4_src=self.EXT_IP,
+                                    ipv4_dst=internal_ip)
+            self.add_flow(datapath, priority_high, match, [])
+            # self.logger.info(f"  Rule Added: Block ICMP Echo Req from {self.EXT_IP} -> {internal_ip}")
+        protocols_to_block = {6: 'TCP', 17: 'UDP'}
+        for proto_num, proto_name in protocols_to_block.items():
+            match1 = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ip_proto=proto_num,
+                                     ipv4_src=self.EXT_IP, ipv4_dst=self.SER_IP)
+            self.add_flow(datapath, priority_high, match1, [])
+            match2 = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ip_proto=proto_num,
+                                     ipv4_src=self.SER_IP, ipv4_dst=self.EXT_IP)
+            self.add_flow(datapath, priority_high, match2, [])
+            # self.logger.info(f"  Rule Added: Block {proto_name} traffic between {self.EXT_IP} and {self.SER_IP}")
+        router_ports_info = self.ROUTER_PORTS.get(dpid, {})
+        all_gateway_ips = [p['ip'] for p in router_ports_info.values()]
+        for in_port_num, port_info in router_ports_info.items():
+            my_gateway_ip = port_info['ip']
+            for target_gw_ip in all_gateway_ips:
+                if my_gateway_ip == target_gw_ip: continue
+                match = parser.OFPMatch(in_port=in_port_num, eth_type=ether_types.ETH_TYPE_IP,
+                                        ip_proto=1, icmpv4_type=icmp.ICMP_ECHO_REQUEST,
+                                        ipv4_dst=target_gw_ip)
+                self.add_flow(datapath, priority_high, match, [])
+                # self.logger.info(f"  Rule Added: Block ICMP Echo Req on Port {in_port_num} to other gateway {target_gw_ip}")
 
-        # Construct flow_mod message and send it
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst)
-        datapath.send_msg(mod)
+        # Rule 4: Block PING (ICMP Echo Request) FROM internal hosts TO ext
+        # (Add this block to match the PDF pingall graphic exactly)
+        # --------------------------------------------------------------------------
+        for internal_ip in self.INTERNAL_HOST_IPS:
+             match = parser.OFPMatch(
+                 eth_type=ether_types.ETH_TYPE_IP,       # IP packet
+                 ip_proto=1,                             # ICMP protocol
+                 icmpv4_type=icmp.ICMP_ECHO_REQUEST,     # Echo Request
+                 ipv4_src=internal_ip,                   # Source is internal host
+                 ipv4_dst=self.EXT_IP                    # Destination is external host
+             )
+             actions = [] # Drop packet
+             self.add_flow(datapath, priority_high, match, actions)
+             self.logger.info(f"  Rule Added: Block ICMP Echo Req from {internal_ip} -> {self.EXT_IP}")
 
-    # Handle the packet_in event
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        
-        msg = ev.msg
-        datapath = msg.datapath
+        msg = ev.msg; datapath = msg.datapath; ofproto = datapath.ofproto; parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']; dpid = datapath.id; pkt = packet.Packet(msg.data); eth = pkt.get_protocol(ethernet.ethernet)
+        if not eth or eth.ethertype == ether_types.ETH_TYPE_LLDP: return
+        if dpid == self.S1_DPID or dpid == self.S2_DPID:
+            self.handle_switch_packet(datapath, msg, in_port, pkt, eth)
+        elif dpid == self.S3_DPID:
+            self.handle_router_packet(datapath, msg, in_port, pkt, eth)
 
-        # Your controller implementation should start here
+    def handle_switch_packet(self, datapath, msg, in_port, pkt, eth):
+        dpid = datapath.id; ofproto = datapath.ofproto; parser = datapath.ofproto_parser; src = eth.src; dst = eth.dst
+        self.mac_to_port.setdefault(dpid, {});
+        # Log only if new/changed
+        # if self.mac_to_port[dpid].get(src) != in_port:
+        #    self.logger.info(f"SWITCH {dpid:x}: Learning MAC {src} is on Port {in_port}")
+        self.mac_to_port[dpid][src] = in_port
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]; actions = [parser.OFPActionOutput(out_port)]; match = parser.OFPMatch(eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions, idle_timeout=20)
+        else: out_port = ofproto.OFPP_FLOOD; actions = [parser.OFPActionOutput(out_port)]
+        data = None;
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER: data = msg.data
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
+    def handle_router_packet(self, datapath, msg, in_port, pkt, eth):
+        if eth.ethertype == ether_types.ETH_TYPE_ARP:
+             arp_pkt = pkt.get_protocol(arp.arp);
+             if arp_pkt: self.handle_arp(datapath, msg, in_port, pkt, eth, arp_pkt); return
+        elif eth.ethertype == ether_types.ETH_TYPE_IP:
+             ip_pkt = pkt.get_protocol(ipv4.ipv4);
+             if ip_pkt: self.handle_ip(datapath, msg, in_port, pkt, eth, ip_pkt); return
+
+    def handle_arp(self, datapath, msg, in_port, pkt, eth, arp_pkt):
+        dpid = datapath.id; ofproto = datapath.ofproto; parser = datapath.ofproto_parser; src_ip = arp_pkt.src_ip; src_mac = arp_pkt.src_mac
+        self.arp_table.setdefault(dpid, {});
+        if self.arp_table[dpid].get(src_ip) != src_mac: # Learn only if new or changed
+            self.arp_table[dpid][src_ip] = src_mac
+            self.logger.info(f"ROUTER {dpid:x}: Learned ARP: {src_ip} -> {src_mac} (from Port {in_port})")
+        target_ip = arp_pkt.dst_ip; my_mac_for_target_ip = None
+        for port_no, port_data in self.ROUTER_PORTS.get(dpid, {}).items():
+             if port_data['ip'] == target_ip: my_mac_for_target_ip = port_data['mac']; break
+        if arp_pkt.opcode == arp.ARP_REQUEST and my_mac_for_target_ip:
+            # self.logger.info(f"ROUTER {dpid:x}: Received ARP Request for my IP {target_ip} from {src_ip}")
+            reply_pkt = packet.Packet(); reply_pkt.add_protocol(ethernet.ethernet(ethertype=ether_types.ETH_TYPE_ARP, dst=src_mac, src=my_mac_for_target_ip))
+            reply_pkt.add_protocol(arp.arp(opcode=arp.ARP_REPLY, src_mac=my_mac_for_target_ip, src_ip=target_ip, dst_mac=src_mac, dst_ip=src_ip))
+            reply_pkt.serialize(); actions = [parser.OFPActionOutput(in_port)]
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER, in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=reply_pkt.data)
+            datapath.send_msg(out)
+            # self.logger.info(f"ROUTER {dpid:x}: Sent ARP Reply: {target_ip} is at {my_mac_for_target_ip}")
+        elif arp_pkt.opcode == arp.ARP_REPLY:
+             if self.arp_table[dpid].get(src_ip) == src_mac: # Check if learning actually happened
+                 self.logger.info(f"ROUTER {dpid:x}: Received ARP Reply from {src_ip} ({src_mac}) - mapping learned/updated.")
+
+    def handle_ip(self, datapath, msg, in_port, pkt, eth, ip_pkt):
+        dpid = datapath.id; ofproto = datapath.ofproto; parser = datapath.ofproto_parser; dst_ip = ip_pkt.dst; src_ip = ip_pkt.src
+        if ip_pkt.ttl <= 1: return
+        my_interface_info = None
+        for port_no, port_data in self.ROUTER_PORTS.get(dpid, {}).items():
+            if port_data['ip'] == dst_ip: my_interface_info = port_data; break
+        if my_interface_info:
+            icmp_pkt = pkt.get_protocol(icmp.icmp)
+            if icmp_pkt and icmp_pkt.type == icmp.ICMP_ECHO_REQUEST:
+                 self.send_icmp_reply(datapath, in_port, pkt, eth, ip_pkt, icmp_pkt)
+            return
+        out_port = self.get_route(dpid, dst_ip)
+        if out_port is None or out_port == in_port: return
+        next_hop_ip = dst_ip; dst_mac = self.arp_table.get(dpid, {}).get(next_hop_ip)
+        if dst_mac:
+            router_out_mac = self.ROUTER_PORTS.get(dpid, {}).get(out_port, {}).get('mac')
+            if not router_out_mac: return
+            actions = [parser.OFPActionDecNwTtl(), parser.OFPActionSetField(eth_src=router_out_mac),
+                       parser.OFPActionSetField(eth_dst=dst_mac), parser.OFPActionOutput(out_port)]
+            match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP, ipv4_dst=dst_ip)
+            self.add_flow(datapath, 5, match, actions, idle_timeout=30)
+            # self.logger.info(f"ROUTER {dpid:x}: Installed routing flow: DstIP={dst_ip} -> Port={out_port}/MAC={dst_mac} (Prio=5)")
+            data = None;
+            if msg.buffer_id == ofproto.OFP_NO_BUFFER: data = msg.data
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=data)
+            datapath.send_msg(out)
+        else:
+            # self.logger.info(f"ROUTER {dpid:x}: No ARP entry for next hop {next_hop_ip} (needed for {dst_ip}). Sending ARP request.")
+            self.send_arp_request(datapath, out_port, next_hop_ip);
+
+    def get_route(self, dpid, dst_ip_str):
+        best_match_port = None
+        longest_prefix = -1
+        try:
+            dst_ip_addr = ipaddress.ip_address(dst_ip_str)
+        except ValueError:
+            # self.logger.error(f"ROUTER {dpid:x}: Invalid destination IP format received: {dst_ip_str}")
+            return None
+        for port_no, port_data in self.ROUTER_PORTS.get(dpid, {}).items():
+            try:
+                network = ipaddress.ip_network(port_data['subnet'], strict=False)
+                if dst_ip_addr in network:
+                    if network.prefixlen > longest_prefix:
+                        longest_prefix = network.prefixlen
+                        best_match_port = port_no
+            except ValueError:
+                 # self.logger.error(f"ROUTER {dpid:x}: Invalid subnet format in ROUTER_PORTS: {port_data.get('subnet', 'N/A')}")
+                 continue
+            except Exception as e: # Fixed typo
+                 self.logger.error(f"ROUTER {dpid:x}: Error processing network {port_data.get('subnet', 'N/A')} for IP {dst_ip_str}: {e}")
+                 continue
+        return best_match_port
+
+    def send_arp_request(self, datapath, out_port, target_ip):
+        dpid = datapath.id; ofproto = datapath.ofproto; parser = datapath.ofproto_parser
+        my_info = self.ROUTER_PORTS.get(dpid, {}).get(out_port);
+        if not my_info: return
+        my_mac = my_info['mac']; my_ip = my_info['ip']
+        req_pkt = packet.Packet(); req_pkt.add_protocol(ethernet.ethernet(ethertype=ether_types.ETH_TYPE_ARP, dst=mac_lib.BROADCAST_STR, src=my_mac))
+        req_pkt.add_protocol(arp.arp(opcode=arp.ARP_REQUEST, src_mac=my_mac, src_ip=my_ip, dst_mac=mac_lib.DONTCARE_STR, dst_ip=target_ip))
+        req_pkt.serialize(); actions = [parser.OFPActionOutput(out_port)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER, in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=req_pkt.data)
+        datapath.send_msg(out)
+        # self.logger.debug(f"ROUTER {dpid:x}: Sent ARP request for {target_ip} via port {out_port}")
+
+    def send_icmp_reply(self, datapath, in_port, pkt_in, eth_in, ipv4_in, icmp_in):
+        dpid = datapath.id; ofproto = datapath.ofproto; parser = datapath.ofproto_parser; my_ip = ipv4_in.dst; my_mac = None
+        for port_no, port_data in self.ROUTER_PORTS.get(dpid, {}).items():
+             if port_data['ip'] == my_ip: my_mac = port_data['mac']; break
+        if not my_mac: return
+        reply_pkt = packet.Packet(); reply_pkt.add_protocol(ethernet.ethernet(ethertype=eth_in.ethertype, dst=eth_in.src, src=my_mac))
+        reply_pkt.add_protocol(ipv4.ipv4(dst=ipv4_in.src, src=my_ip, proto=ipv4_in.proto, ttl=64))
+        reply_pkt.add_protocol(icmp.icmp(type_=icmp.ICMP_ECHO_REPLY, code=icmp.ICMP_ECHO_REPLY_CODE, csum=0, data=icmp_in.data))
+        reply_pkt.serialize(); actions = [parser.OFPActionOutput(in_port)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER, in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=reply_pkt.data)
+        datapath.send_msg(out)
+        # self.logger.info(f"ROUTER {dpid:x}: Sent ICMP Echo Reply from {my_ip} to {ipv4_in.src}")
--- lab1/run_network.py
+++ lab1/run_network.py
@@ -1,52 +1,117 @@
+# File: run_network.py
+
+#!/usr/bin/env python3
 """
- Copyright 2024 Computer Networks Group @ UPB
+run_network.py: Creates the network topology for Lab 1 using Mininet.
+Based on the topology described in Figure 1 of ANS_SS25_Lab1.pdf.
+"""
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+# Import necessary parts from the Mininet library
+from mininet.net import Mininet                      # The main class for creating network topologies
+from mininet.node import Controller, RemoteController, OVSKernelSwitch # Node types: Controller, External Controller, Open vSwitch
+from mininet.cli import CLI                          # Command Line Interface for interacting with the network
+from mininet.log import setLogLevel, info            # For printing informational messages
+from mininet.link import TCLink                      # Link type that allows setting properties like bandwidth/delay
 
-      https://www.apache.org/licenses/LICENSE-2.0
+def create_network():
+    """Creates and configures the Mininet network."""
+    info('* Starting network creation *\n')
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- """
+    # 1. Define the Controller
+    # This tells Mininet that the controller (the "brain") is running outside
+    # of Mininet itself. We specify its IP address and the port it listens on.
+    # Ryu's default port is 6653 (even though the diagram shows 6633 sometimes).
+    info('--- Defining Controller (Ryu expected at 127.0.0.1:6653)...\n')
+    c1 = RemoteController( name='c1',        # Give the controller a name
+                           ip='127.0.0.1',  # Standard localhost IP
+                           port=6653 )      # Standard OpenFlow port Ryu uses
 
-#!/bin/env python3
+    # 2. Create the main Mininet Network object
+    # We pass the controller object 'c1' to the network.
+    # switch=OVSKernelSwitch: Use Open vSwitch (a software switch) for s1, s2, s3.
+    # link=TCLink: Use links where we can set traffic control parameters (bw, delay).
+    # autoSetMacs=True: Let Mininet assign unique MAC addresses to hosts.
+    # autoStaticArp=False: Disable Mininet's automatic ARP table entries; our controller will handle ARP.
+    info('--- Creating Mininet object with OVS switches and TC links...\n')
+    net = Mininet( controller=c1,
+                   switch=OVSKernelSwitch,
+                   link=TCLink,
+                   autoSetMacs=True,
+                   autoStaticArp=False )
 
-from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.node import RemoteController, OVSKernelSwitch
-from mininet.link import TCLink
-from mininet.cli import CLI
-from mininet.log import setLogLevel
+    # 3. Add Hosts (Virtual Computers)
+    # net.addHost(name, ip, defaultRoute) creates each host.
+    # The IP address includes the subnet mask length (/24).
+    # defaultRoute tells the host where to send packets destined for other networks.
+    info('--- Adding Hosts (h1, h2, ser, ext)...\n')
+    h1 = net.addHost('h1', ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
+    h2 = net.addHost('h2', ip='10.0.1.3/24', defaultRoute='via 10.0.1.1')
+    ser = net.addHost('ser', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
+    ext = net.addHost('ext', ip='192.168.1.123/24', defaultRoute='via 192.168.1.1')
 
+    # 4. Add Switches (s1, s2) and the Router (s3)
+    # net.addSwitch(name, dpid) creates the switches/router.
+    # We treat s3 as a switch here; the controller makes it act like a router.
+    # dpid (Datapath ID) is a unique hexadecimal identifier for the controller.
+    info('--- Adding Switches (s1, s2) and Router (s3)...\n')
+    s1 = net.addSwitch('s1', dpid='0000000000000011') # Use simple DPIDs
+    s2 = net.addSwitch('s2', dpid='0000000000000012')
+    s3 = net.addSwitch('s3', dpid='0000000000000013') # The router device
 
-class NetworkTopo(Topo):
+    # 5. Create Links (Virtual Cables)
+    # net.addLink(node1, node2, port1, port2, **options) connects the devices.
+    # link_opts defines bandwidth (bw) and delay based on the PDF.
+    # Specifying port numbers (e.g., port1=3, port2=1) helps map the physical
+    # connections to the logical port numbers used in the controller (ROUTER_PORTS).
+    info('--- Creating Links with specified BW and Delay...\n')
+    link_opts = {'bw': 15, 'delay': '10ms'} # 15 Mbps bandwidth, 10ms delay
 
-    def __init__(self):
+    # Connect h1 and h2 to switch s1 (Mininet auto-assigns s1 ports, likely 1 and 2)
+    net.addLink(h1, s1, **link_opts)
+    net.addLink(h2, s1, **link_opts)
 
-        Topo.__init__(self)
+    # Connect server ser to switch s2 (Mininet auto-assigns s2 port, likely 1)
+    net.addLink(ser, s2, **link_opts)
 
-        # Build the specified network topology here
+    # Connect the switches and external host to the router (s3) using specific ports
+    # Router Port 1 <-> Switch s1 (using s1's port 3, assumes ports 1,2 used for hosts)
+    net.addLink(s1, s3, port1=3, port2=1, **link_opts)
+    # Router Port 2 <-> Switch s2 (using s2's port 2, assumes port 1 used for server)
+    net.addLink(s2, s3, port1=2, port2=2, **link_opts)
+    # Router Port 3 <-> External host ext (using ext's port 1, assumed)
+    net.addLink(ext, s3, port1=1, port2=3, **link_opts)
 
-def run():
-    topo = NetworkTopo()
-    net = Mininet(topo=topo,
-                  switch=OVSKernelSwitch,
-                  link=TCLink,
-                  controller=None)
-    net.addController(
-        'c1', 
-        controller=RemoteController, 
-        ip="127.0.0.1", 
-        port=6653)
-    net.start()
+    # 6. Start the Network
+    # net.build() creates the network objects based on the definitions above.
+    info('* Building network simulation *\n')
+    net.build()
+
+    # Start the controller first (although it's external, this ensures Mininet knows about it)
+    # info('--- Starting controller connection process ---\n')
+    # c1.start() # Often not needed explicitly for RemoteController
+
+    # Start the switches/router, making them connect to the controller 'c1'
+    info('--- Starting switches and router (s1, s2, s3) ---\n')
+    net.get('s1').start([c1])
+    net.get('s2').start([c1])
+    net.get('s3').start([c1])
+
+    # Note: The actual gateway IPs (10.0.1.1, 10.0.2.1, 192.168.1.1) are logical
+    # addresses managed by the Ryu controller. They are NOT assigned directly to
+    # the 's3' switch's interfaces within Mininet itself. Hosts just use them
+    # as their 'defaultRoute'.
+
+    # 7. Run the Command Line Interface (CLI)
+    # This provides the 'mininet>' prompt for interaction (ping, iperf, etc.).
+    info('* Running Mininet CLI (type "exit" to quit) *\n')
     CLI(net)
+
+    # 8. Stop the Network
+    # Cleans up the virtual interfaces and processes when CLI exits.
+    info('* Stopping network simulation *\n')
     net.stop()
 
+# Standard Python entry point check
 if __name__ == '__main__':
-    setLogLevel('info')
-    run()
+    setLogLevel('info') # Set Mininet's logging level (info, debug, warning, etc.)
+    create_network()    # Call the main function to build the network
