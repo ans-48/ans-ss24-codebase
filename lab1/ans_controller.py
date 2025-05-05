b60eea8de0545b818a3f1b35ecf04b74ff999dee
Selorm Lotsu <selorml@mail.uni-paderborn.de>
2025-05-05 13:53:43 +0200

    Implement switch

--- lab1/ans_controller.py
+++ lab1/ans_controller.py
@@ -19,7 +19,8 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
 
 class LearningSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -28,7 +29,19 @@ class LearningSwitch(app_manager.RyuApp):
         super(LearningSwitch, self).__init__(*args, **kwargs)
 
         # Here you can initialize the data structures you want to keep at the controller
-        
+        # Router port MACs assumed by the controller
+        self.port_to_own_mac = {
+            # 1: "00:00:00:00:01:01",
+            # 2: "00:00:00:00:01:02",
+            # 3: "00:00:00:00:01:03"
+        }
+
+        # Router port (gateways) IP addresses assumed by the controller
+        self.port_to_own_ip = {
+            1: "10.0.1.1",
+            2: "10.0.2.1",
+            3: "192.168.1.1"
+        }
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -62,3 +75,32 @@ class LearningSwitch(app_manager.RyuApp):
         datapath = msg.datapath
 
         # Your controller implementation should start here
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        self.port_to_own_mac.setdefault(datapath.id, {})
+        pkt = packet.Packet(msg.data)
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+        dst = eth_pkt.dst
+        src = eth_pkt.src
+
+        in_port = msg.match['in_port']
+        self.logger.info("packet in %s %s %s %s", datapath.id, src, dst, in_port)
+        self.port_to_own_mac[datapath.id][src] = in_port
+
+        if dst in self.port_to_own_mac[datapath.id]:
+            out_port = self.port_to_own_mac[datapath.id][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+
+        out = parser.OFPPacketOut(datapath=datapath,
+                                           buffer_id=ofproto.OFP_NO_BUFFER,
+                                           in_port=in_port, actions=actions,
+                                           data=msg.data)
+        datapath.send_msg(out)
